58f8e0bdef619782ebc7976b168089ef26ed301c
904556575lwh <65028024+904556575lwh@users.noreply.github.com>
2020-06-25 18:51:54 +0900

    Update db.php

--- db.php
+++ db.php
@@ -42,6 +42,7 @@ Last Access Time<br><br>
         <?=escape1($a)?><br>
 <?php endforeach; ?>
 <?php echo 'Japan time : ' . date("Y/m/d H:i:s"); ?>
+<?php echo '<p style="color: red">By LiWenhua</p>'; ?>
 </body>
 </html>
 
